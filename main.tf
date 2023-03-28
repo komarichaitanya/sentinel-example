337b80ef3930708b391d8342f5ddd3a88b17a5d6
komarichaitanya <kchaitanya981@gmail.com>
2023-03-28 16:21:16 +0530

    firstcommit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">0.1"
+  required_providers {
+    azurerm={
+        source="hashicorp/azurerm"
+        version = ">2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "aea9e16e-cfec-4e04-a6aa-659cc94eb92c"
+  tenant_id = "da7f67e1-1fae-4e2f-a268-f5142bae8320"
+  client_id = "6578ca00-379b-4ce4-b6d0-b23633b7a3c4"
+  client_secret = "5Dc8Q~c8ld4IO9N_WrrW4v8ODZQQ15VHxTxANb6w"
+}
+resource "azurerm_resource_group" "exp" {
+  name = "demo"
+  location = "east US"
+}
